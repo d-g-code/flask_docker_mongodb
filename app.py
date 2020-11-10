efb4f59df43e0d0a7222463644df94fd1e25b15d
d-g-code <>
2020-11-10 18:15:32 +0100

    added database mongodb, modified html added input form

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request, render_template
+from pymongo import MongoClient
+import os
 
 
 app = Flask(__name__)
 
 
+client = MongoClient(
+    os.environ['DB_PORT_27017_TCP_ADDR'],
+    27017)
+db = client.tododb
+
 @app.route('/')
 def todo():
-    return "Hello"
+
+    todo_data = db.tododb.find()
+    items = [item for item in todo_data]
+
+    return render_template('todo.html', items=items)
 
 
 @app.route('/new', methods=['POST'])
 def new():
+
+    item_doc = {
+        'name': request.form['name'],
+        'description': request.form['description']
+    }
+    db.tododb.insert_one(item_doc)
+
     return redirect(url_for('todo'))
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
